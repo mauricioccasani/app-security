cded0cf2478acd0d8f0aa84cccfe3a7a5cbbdaec
Mauricio Ccasani Olivares <mccasani@emeal.nttdata.com>
2023-06-02 00:29:33 -0500

    Contum app security

--- src/main/java/pe/com/ccasani/appsecurity/security/filter/SecurityConfiguration.java
+++ src/main/java/pe/com/ccasani/appsecurity/security/filter/SecurityConfiguration.java
@@ -1,23 +1,35 @@
 package pe.com.ccasani.appsecurity.security.filter;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import pe.com.ccasani.appsecurity.security.athentication.CustomAuthenticationProvider;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Slf4j
+@RequiredArgsConstructor
 @Configuration
 public class SecurityConfiguration {
     @Bean
     BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    private final CustomAuthenticationProvider authenticationProvider;
+
+    private  AuthenticationManager authenticationManager(){
+        return new ProviderManager(this.authenticationProvider);
     }
 
     @Bean
@@ -32,23 +44,9 @@ public class SecurityConfiguration {
                         .anyRequest()
                         .authenticated())
                 .httpBasic(withDefaults());
+        http.addFilter(new BasicAuthenticationFilter(this.authenticationManager()));
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password(this.bCryptPasswordEncoder().encode("abc"))
-                .roles("USER")
-                .build();
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password(this.bCryptPasswordEncoder().encode("abc"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
 
 }
